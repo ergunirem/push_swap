ee3a9e4f8c7d3647f28221325975789cd0d978cb
ergunirem <iremthedeveloper@gmail.com>
2021-05-28 21:36:39 +0200

    lstmax & lstmin added

--- helper.c
+++ helper.c
@@ -6,13 +6,13 @@
 /*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/25 13:39:42 by icikrikc      #+#    #+#                 */
-/*   Updated: 2021/05/25 13:56:38 by icikrikc      ########   odam.nl         */
+/*   Updated: 2021/05/28 20:01:17 by icikrikc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack	*lst_new_node(int num)
+t_stack	*ft_lst_new_node(int num)
 {
 	t_stack	*new;
 
@@ -22,4 +22,42 @@ t_stack	*lst_new_node(int num)
 	new->num = num;
 	new->next = NULL;
 	return (new);
+}
+
+t_stack	*ft_lstmax(t_stack **stack)
+{
+	t_stack	*tmp_max;
+	t_stack	*tmp;
+
+	tmp = *stack;
+	tmp_max = tmp;
+	if(tmp)
+	{
+		while (tmp && tmp->next)
+		{
+			if (tmp->next->num > tmp_max->num)
+				tmp_max = tmp->next;
+			tmp = tmp->next;
+		}
+	}
+	return (tmp_max);
+}
+
+t_stack	*ft_lstmin(t_stack **stack)
+{
+	t_stack	*tmp_min;
+	t_stack	*tmp;
+
+	tmp = *stack;
+	tmp_min = tmp;
+	if(tmp)
+	{
+		while (tmp && tmp->next)
+		{
+			if (tmp->next->num < tmp_min->num)
+				tmp_min = tmp->next;
+			tmp = tmp->next;
+		}
+	}
+	return (tmp_min);
 }
