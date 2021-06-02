6bfa09da1e27b87c03c15702273ae3565722e6d7
ergunirem <iremthedeveloper@gmail.com>
2021-06-02 21:32:09 +0200

    moved sort_three&four&five to new file and minor changes

---
+++ sort_mini.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   sort_mini.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/06/02 13:35:47 by icikrikc      #+#    #+#                 */
+/*   Updated: 2021/06/02 20:31:04 by icikrikc      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	sort_three(t_stack **stack_a, t_all *all)
+{
+	int	max;
+
+	max = ft_lstmax(stack_a)->num;
+	while (!is_sorted((*stack_a)))
+	{
+		if ((*stack_a)->num == max)
+			ra(stack_a, all);
+		else
+			sa(stack_a);
+	}
+}
+
+void	sort_four(t_stack **stack_a, t_stack **stack_b, t_all *all)
+{
+	int	min;
+
+	min = ft_lstmin(stack_a)->num;
+	while ((*stack_a)->num != min)
+	{
+		ra(stack_a, all);
+	}
+	pb(stack_a, stack_b, all);
+	sort_three(stack_a, all);
+	pa(stack_a, stack_b, all);
+}
+
+void	sort_five(t_stack **stack_a, t_stack **stack_b, t_all *all)
+{
+	int	min;
+
+	min = ft_lstmin(stack_a)->num;
+	while ((*stack_a))
+	{
+		if ((*stack_a)-> num == min)
+		{
+			pb(stack_a, stack_b, all);
+			break ;
+		}
+		ra(stack_a, all);
+	}
+	min = ft_lstmin(stack_a)->num;
+	while ((*stack_a))
+	{
+		if ((*stack_a)-> num == min)
+		{
+			pb(stack_a, stack_b, all);
+			break ;
+		}
+		ra(stack_a, all);
+	}
+	sort_three(stack_a, all);
+	pa(stack_a, stack_b, all);
+	pa(stack_a, stack_b, all);
+}
